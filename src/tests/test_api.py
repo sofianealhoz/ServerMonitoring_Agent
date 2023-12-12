42ed29a3c23cab19256d929a70c2a604541a03c9
EZZAR <ahmedezzar@outlook.fr>
2023-12-12 16:04:08 +0100

    Ajout des tests unitaires pour le Process et pour le Hdd

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -2,6 +2,7 @@
 import threading
 import os
 import sys
+import psutil
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -9,6 +10,7 @@ from fastapi.testclient import TestClient
 from server import app
 from monitor import MonitorTask
 from src.monitor.LogFunction import count_unique_users, error404
+from src.domain.models.hdd import Hdd
 
 
 class MonitorTaskFake(MonitorTask):
@@ -19,6 +21,13 @@ class MonitorTaskFake(MonitorTask):
     and make deterministic test (deterministic = repeatable and known values)
     """
     interval: int = 0
+    harddrive_usage: Hdd = Hdd(
+        total=62.688777923583984,
+        used=8.715827941894531,
+        free=50.75762939453125,
+        percent=14.7
+    )
+
     cpu_percent: list[float] = ["10", "12"]
     num_cores: int = 3
 
@@ -48,6 +57,33 @@ def test_get_cpu_usage():
     # restore monitortask for next test
     app.state.monitortask = save_app
 
+
+def test_get_hdd():
+    # backup of the existing monitortask to restore it after the test
+    save_app = app.state.monitortask
+    # use fake monitor to have deterministic values
+    app.state.monitortask = MonitorTaskFake()
+    
+    response = client.get("/usageHdd")
+    
+    # Check status code
+    assert response.status_code == 200
+    
+    # Check response format
+    assert isinstance(response.json(), dict), f"Expected a dictionary in response: {response.json()}"
+    
+    # Check keys in the response
+    expected_keys = ["total", "used", "free", "percent"]
+    assert all(key in response.json() for key in expected_keys), f"Expected keys {expected_keys} in the response: {response.json()}"
+    
+    # Access the Hdd instance directly
+    hdd_instance = app.state.monitortask.harddrive_usage
+    
+    # Check the type of the created instance
+    assert isinstance(hdd_instance, Hdd), f"Expected an instance of Hdd, but got {type(hdd_instance)}"
+    
+    # restore monitortask for the next test
+    app.state.monitortask = save_app
 
 def test_get_cpu_core():
     response = client.get("/metrics/v1/cpu/core")
@@ -109,6 +145,35 @@ def test_get_network_usage():
     app.state.monitortask = save_app
 
 
+def test_get_process_usage():
+    # backup of the existing monitortask to restore it after the test
+    save_app = app.state.monitortask
+    # use fake monitor to have deterministic values
+    app.state.monitortask = MonitorTaskFake()
+    
+    response = client.get("/usageProcess")
+    
+    # Check status code
+    assert response.status_code == 200
+    
+    # Check response format
+    assert isinstance(response.json(), list), f"Expected a list in response: {response.json()}"
+    
+    # Check each object in the list
+    for process_info in response.json():
+        assert isinstance(process_info, dict), f"Expected each item in the list to be a dictionary: {response.json()}"
+        assert all(key in process_info for key in ["pid", "name", "rss", "cpu_percent"]), f"Expected keys 'pid', 'name', 'rss', 'cpu_percent' in each item: {process_info}"
+        
+        # Check the type of values in each object
+        assert isinstance(process_info["pid"], int), f"Expected 'pid' to be an int: {process_info}"
+        assert isinstance(process_info["name"], str), f"Expected 'name' to be a string: {process_info}"
+        assert isinstance(process_info["rss"], float), f"Expected 'rss' to be a float: {process_info}"
+        assert isinstance(process_info["cpu_percent"], float), f"Expected 'cpu_percent' to be a float: {process_info}"
+    
+    # restore monitortask for the next test
+    app.state.monitortask = save_app
+
+
 def test_log_functions():
     log_file_path = "src/monitor/Documents"
     
@@ -119,6 +184,20 @@ def test_log_functions():
     # Test error404
     count_404 = error404(log_file_path)
     assert count_404 == 2, f"Expected 2 occurrences of 404 errors, but got {count_404}"
+
+    # backup of the existing monitortask to restore it after the test
+    save_app = app.state.monitortask
+    # use fake monitor to have deterministic values
+    app.state.monitortask = MonitorTaskFake()
+    
+    response = client.get("/logMessage")
+    # Check status code
+    assert response.status_code == 200
+
+    # restore monitortask for the next test
+    app.state.monitortask = save_app
+
+    
 
 
 
